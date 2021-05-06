bdcdad126cd20ad7fab23ca80850d4d206a48042
Manoj Randeni <randeni.ramr@gmail.com>
2021-05-06 11:09:58 +0530

    Create pop method

--- Stack/src/Stack.java
+++ Stack/src/Stack.java
@@ -6,7 +6,16 @@ public class Stack {
         int[] newNumArray = new int[numArrayLength + 1];
         numberArray[numArrayLength + 1] = number;
         numberArray = newNumArray;
+        numArrayLength = newNumArray.length;
     }
 
+    public void pop() {
+        int[] newNumArray = new int[numArrayLength - 1];
+        int newNumArrayLength = newNumArray.length;
+        for (int i = 0; i < newNumArrayLength; i++) {
+            newNumArray[i] = numberArray[i];
+        }
+        numberArray = newNumArray;
+    }
 
 }
