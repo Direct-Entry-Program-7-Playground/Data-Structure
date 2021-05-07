eb59ba218f14c87a8e80d7fab673eef0fd779fa1
Manoj Randeni <randeni.ramr@gmail.com>
2021-05-07 10:10:40 +0530

    Create enqueueRear method

--- Deque/src/ds/Deque.java
+++ Deque/src/ds/Deque.java
@@ -1,7 +1,21 @@
 package ds;
 
 public class Deque extends Queue {
-
+public void enqueueRear(int number){
+    if(this.empty()){
+        queueArray = new int[1];
+        queueArray[0] = number;
+    }else {
+        int queueArrayLength = queueArray.length;
+        int[] newQueueArray = new int[queueArrayLength + 1];
+        int newQueueArrayLength = newQueueArray.length;
+        newQueueArray[0] = number;
+        for (int i = 1; i < newQueueArrayLength; i++) {
+            newQueueArray[i] = queueArray[i-1];
+        }
+        queueArray = newQueueArray;
+    }
+}
 }
 
 
