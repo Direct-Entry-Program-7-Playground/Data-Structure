d554de534cec9f39cb8a4a19878b035f5f094fbd
Manoj Randeni <randeni.ramr@gmail.com>
2021-05-07 10:22:24 +0530

    Create dequeueFront method

--- Deque/src/DequeTest.java
+++ Deque/src/DequeTest.java
@@ -1,2 +1,61 @@
+import ds.Deque;
+
 public class DequeTest {
+    public static void main(String[] args) {
+        Deque deque = new Deque();
+
+        System.out.println(deque.size());
+        System.out.println(deque.empty());
+
+        deque.enqueue(10);
+        deque.enqueueRear(15);
+        deque.enqueueRear(25);
+
+        deque.enqueue(20);
+        deque.enqueue(30);
+
+        System.out.println(deque.peek());
+
+    /*    deque.dequeue();
+        deque.dequeue();
+        deque.dequeue();
+        deque.dequeue();
+        deque.dequeue();*/
+
+        System.out.println(deque.size());
+        System.out.println(deque.empty());
+//        System.out.println(deque.peek());
+        deque.print();
+
+        deque.enqueue(40);
+        deque.enqueue(50);
+        deque.enqueue(60);
+
+        System.out.println(deque.contains(30));
+        System.out.println(deque.contains(50));
+        System.out.println(deque.contains(25));
+
+        deque.clear();
+
+        System.out.println(deque.size());
+        System.out.println(deque.empty());
+//        System.out.println(deque.peek());
+        deque.print();
+
+        deque.enqueue(70);
+        deque.enqueue(80);
+        deque.enqueue(90);
+        deque.enqueue(100);
+
+        System.out.println(deque.size());
+        System.out.println(deque.empty());
+        System.out.println(deque.peek());
+        deque.print();
+
+        deque.dequeueFront();
+        System.out.println(deque.size());
+        System.out.println(deque.empty());
+        System.out.println(deque.peek());
+        deque.print();
+    }
 }
--- Deque/src/ds/Deque.java
+++ Deque/src/ds/Deque.java
@@ -1,21 +1,35 @@
 package ds;
 
 public class Deque extends Queue {
-public void enqueueRear(int number){
-    if(this.empty()){
-        queueArray = new int[1];
-        queueArray[0] = number;
-    }else {
-        int queueArrayLength = queueArray.length;
-        int[] newQueueArray = new int[queueArrayLength + 1];
-        int newQueueArrayLength = newQueueArray.length;
-        newQueueArray[0] = number;
-        for (int i = 1; i < newQueueArrayLength; i++) {
-            newQueueArray[i] = queueArray[i-1];
+    public void enqueueRear(int number) {
+        if (this.empty()) {
+            queueArray = new int[1];
+            queueArray[0] = number;
+        } else {
+            int queueArrayLength = queueArray.length;
+            int[] newQueueArray = new int[queueArrayLength + 1];
+            int newQueueArrayLength = newQueueArray.length;
+            newQueueArray[0] = number;
+            for (int i = 1; i < newQueueArrayLength; i++) {
+                newQueueArray[i] = queueArray[i - 1];
+            }
+            queueArray = newQueueArray;
         }
-        queueArray = newQueueArray;
     }
-}
+
+    public void dequeueFront() {
+        if (!this.empty()) {
+            int queueArrayLength = queueArray.length;
+            int[] newQueueArray = new int[queueArrayLength - 1];
+            int newQueueArrayLength = newQueueArray.length;
+            for (int i = 0; i < newQueueArrayLength; i++) {
+                newQueueArray[i] = queueArray[i ];
+            }
+            queueArray = newQueueArray;
+        } else {
+            System.out.println("Empty queue, nothing to remove!");
+        }
+    }
 }
 
 
